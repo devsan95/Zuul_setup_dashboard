32cbef205779ea47e819229a70bafc5143f57ef8
zhxie <zhiqi.xie@nokia.com>
2017-03-29 16:34:26 +0800

    add docker test scripts
    Change-Id: Iff1dbd97ea4fae5a14b4063516777bb9efdf8b68

---
+++ tools/prepare_docker_validation.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# prepare env
+export GIT_SSL_NO_VERIFY=true
+SCRIPT_DIR="`dirname \"${BASH_SOURCE[0]}\"`"
+SCRIPT_DIR="`( cd \"$SCRIPT_DIR/..\" && pwd )`"  # absolutized and normalized
+if [ -z "$SCRIPT_DIR" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  echo "Can't find CIHOME Path"
+  exit 1  # fail
+fi
+script_pwd=update_zuul_layout
+work_path=/tmp/zuul_tmp/${script_pwd}/
+mkdir -p /tmp/zuul_tmp/${script_pwd}/
+rm -rf /tmp/zuul_tmp/${script_pwd}/*
+OLD_PATH="`pwd`"
+cd ${work_path}
+git clone http://gerrit.app.alcatel-lucent.com/gerrit/MN/SCMTA/zuul/layout
+
+# update conf
+${SCRIPT_DIR}/tools/update_zuul_qa_config.sh
+sed -i 's#zuul.zuulqa.dynamic.nsn-net.net#zuul-docker.zuulqa2.dynamic.nsn-net.net#g' /etc/zuul/zuul.conf
+
+#update layout
+cat <<EOF > ${work_path}/layout/layout.d/docker_test.yaml
+projects:
+  - name: qa_dummy_job_docker
+    check:
+      - qa_download_patch_set
+    gate:
+      - qa_always_succeed
+    post:
+      - qa_always_succeed
+EOF
+
+. ${SCRIPT_DIR}/pyenv.sh
+python ${SCRIPT_DIR}/layout/layout_handler.py -i "${work_path}/layout/layout.yaml" \
+ -z "/etc/zuul/zuul.conf" merge -o "/etc/zuul/layout.etc"
+
+#restart service
+supervisorctl restart zuul-launcher
+supervisorctl restart zuul-merger
+supervisorctl restart zuul-server
---
+++ tools/run_docker_validation.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# update known hosts
+echo '[gerrit.zuulqa.dynamic.nsn-net.net]:29418,[10.181.54.157]:29418 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCrU2RQq8bq4DT9Rixebtt/BksVCauQECaBQmtO83ffkWzbHA18pgqRd6/EpSI3wLilUWUQ5ET4k4oNicQDv3bBrOkG0ZADHZ9vE2WMG2y4IqQLNf72gF5IHpxrnapU+EmEEALbHtuDzHJusO6z6C/yyJFRiEDs4KNsEMfFckQLOQ==' >> ~/.ssh/known_hosts
+
+# update git name
+git config --global user.email '5g_hz.scm@nokia.com'
+git config --global user.name 'admin'
+
+# run validation
+. /root/mn_scripts/pyenv.sh
+python /root/mn_scripts/pipeline/validate_pipeline.py -p dummy-project-docker
