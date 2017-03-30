b3e822ade89ed480522fcf074b8bfc1b0e6989da
zhxie <zhiqi.xie@nokia.com>
2017-03-30 17:42:54 +0800

    delete known hosts
    Change-Id: Ib73871460226aac824fb4b018644074975e0723c

--- tools/run_docker_validation.sh
+++ tools/run_docker_validation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # update known hosts
-echo '[gerrit.zuulqa.dynamic.nsn-net.net]:29418,[10.181.54.157]:29418 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCrU2RQq8bq4DT9Rixebtt/BksVCauQECaBQmtO83ffkWzbHA18pgqRd6/EpSI3wLilUWUQ5ET4k4oNicQDv3bBrOkG0ZADHZ9vE2WMG2y4IqQLNf72gF5IHpxrnapU+EmEEALbHtuDzHJusO6z6C/yyJFRiEDs4KNsEMfFckQLOQ==' >> ~/.ssh/known_hosts
+# echo '[gerrit.zuulqa.dynamic.nsn-net.net]:29418,[10.181.54.157]:29418 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCrU2RQq8bq4DT9Rixebtt/BksVCauQECaBQmtO83ffkWzbHA18pgqRd6/EpSI3wLilUWUQ5ET4k4oNicQDv3bBrOkG0ZADHZ9vE2WMG2y4IqQLNf72gF5IHpxrnapU+EmEEALbHtuDzHJusO6z6C/yyJFRiEDs4KNsEMfFckQLOQ==' >> ~/.ssh/known_hosts
 
 # update git name
 git config --global user.email '5g_hz.scm@nokia.com'
