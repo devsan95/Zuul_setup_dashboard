9d0f13b5db8f0ef60db74a435cd13b69818b1974
zhxie <zhiqi.xie@nokia-sbell.com>
2017-08-08 14:50:17 +0800

    fix gerrit api error
    Change-Id: I8d0ea649c5740c20482da85d4efbf5e79cd41328

--- pipeline/validate_pipeline.py
+++ pipeline/validate_pipeline.py
@@ -72,7 +72,7 @@ def _main():
         raise Exception('Patchset has not been checked')
 
     gerrit_api.review_patch_set(
-        ssh_user, ssh_server, patchset, ['label:Code-Review=+2'])
+        ssh_user, ssh_server, patchset, ['Code-Review=+2'])
 
     for i in range(1, 20):
         print('Waiting for 5 secs...')
