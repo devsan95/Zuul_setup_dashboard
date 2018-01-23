0f54cea0ae037aa02c7059f963459bb1475c7e9f
dxuan <dongsheng.xuan@nokia-sbell.com>
2018-01-23 13:26:28 +0800

    add tools/compare_layout.sh to compare layout.yaml after merged
    Change-Id: Ifca68ebbc773fa2e03f2874b269b92429e7f4310

---
+++ tools/compare_layout.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+export HTTP_PROXY=
+export HTTPS_PROXY=
+export http_proxy=
+export https_proxy=
+
+. mn_scripts/pyenv.sh
+
+python mn_scripts/layout/layout_handler.py -i "layout/layout.yaml" -z "conf/zuul_conf/zuul.conf" merge -o "layout.yaml"
+chmod 777 layout.yaml
+line1=$(cat layout.yaml |head -3 |tail -1)
+line2=$(cat /var/fpwork/zuul_prod/etc/layout.yaml |head -3 |tail -1)
+if [ "l$line1" == "l$line2" ];then
+    echo "same content layout.yaml"
+    exit1
+else
+    cp -f layout.yaml /var/fpwork/zuul_prod/etc/layout.yaml
+    echo "layout.yaml is different, updated!"
+fi
+sudo docker exec zuul-server bash -c 'kill -SIGHUP `supervisorctl pid zuul-server`'
