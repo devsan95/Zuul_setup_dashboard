06eb35f7a90d2fa22333f753caeabb7549c6060a
lingyuji <lingyun-alex.jiang@nokia-sbell.com>
2019-02-27 13:26:54 +0530

    add mysql api
    Change-Id: Icc555f51f4ea69f632c3bf034fc0325f9369483a

---
+++ CITOOLS/api/mysql_api.py
@@ -0,0 +1,127 @@
+import yaml
+import mysql.connector
+
+
+def init_from_yaml(yaml_path, server_name):
+    """
+    Init database instance from yaml config file
+    :param ymal_path: path of yaml file
+    :type ymal_path: basestring
+    :param server_name: server name of mysql database
+    :type server_name: basestring
+    :return: MysqlConnector instance
+    """
+    with open(yaml_path) as yaml_file:
+        obj = yaml.load(yaml_file)
+        server = obj[server_name]
+        host = server['host']
+        user = server['user']
+        passwd = server['passwd']
+    return MysqlConnector(host=host, user=user, passwd=passwd)
+
+
+class MysqlConnector(object):
+
+    def __init__(self, host, user, passwd, database=None):
+        """
+        init method
+        :param host: mysql host address
+        :type host: basestring
+        :param user: database username
+        :type user: basestring
+        :param passwd: database password
+        :type passwd: basestring
+        :param database: database name
+        :type database: basestring
+        """
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        self.database = database
+        self.mydb = None
+        self.init_server()
+
+    def init_server(self):
+        """
+        Init mysql server connection
+        :return:
+        """
+        self.mydb = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            passwd=self.passwd,
+            database=self.database
+        ) if self.database else mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            passwd=self.passwd
+        )
+
+    def init_database(self, database):
+        """
+        Re init mysql database connection to specific database
+        :param database: database name
+        :type database: basestring
+        :return:
+        """
+        self.database = database
+        self.init_server()
+
+    def executor(self, sql, val=None, commit=False, output=False):
+        """
+        Execute
+        :param sql: sql command
+        :type sql: basestring
+        :param val: value for data insert
+        :type val: tuple
+        :param commit: commit to database or not
+        :type commit: bool
+        :return:
+        """
+        mycursor = self.mydb.cursor()
+        print("Executing SQL command: {0}".format(sql))
+        if val:
+            print("Value: {0}".format(val))
+        mycursor.execute(sql, val)
+        if commit:
+            self.mydb.commit()
+        if output:
+            return mycursor.fetchall()
+        return None
+
+    def insert_info(self, table, values):
+        """
+        Insert data in to table of database
+        :param table: table name
+        :type table: basestring
+        :param values: values of each columns
+        :type values: dict
+        :return:
+        """
+        sql = "INSERT INTO {table} ({column}) VALUE ({extend})".format(
+            table=table,
+            column=','.join(values.keys),
+            extend=','.join(['%s' for i in range(len(values.keys))])
+        )
+        value = (values[column] for column in values.keys)
+        self.executor(sql, val=value, commit=True)
+
+    def update_info(self, table, replacements, conditions):
+        """
+        update value of mysql database table
+        :param table: table name
+        :type table: basestring
+        :param replacements: which values needed to updated to
+        :type replacements: dict
+        :param conditions: conditon for which row need to be updated
+        :type conditions: dict
+        :return:
+        """
+        sql = "UPDATE {table} SET {replacement} WHERE {conditions}".format(
+            table=table,
+            replacement=', '.join(["{key} = '{value}'".format(key=key, value=value)
+                                  for key, value in replacements.items()]),
+            conditions='AND '.join(["{name} = '{condition}'".format(name=name, condition=condition)
+                                    for name, condition in conditions.items()])
+        )
+        self.executor(sql, commit=True)
---
+++ integration/skytrack_database_handler.py
@@ -0,0 +1,92 @@
+import re
+
+import fire
+
+from api import gerrit_rest
+from api import mysql_api
+from update_oam_commit import get_description
+
+
+def get_jira_id(integration_change, gerrit_info_path):
+    print("Getting JIRA ID from {0} commit message".format(integration_change))
+    rest = gerrit_rest.init_from_yaml(gerrit_info_path)
+    rest.init_cache(1000)
+    description = get_description(rest, integration_change)
+    jira_key = ''
+    for line in description.split('\n'):
+        match = re.findall(r'\%JR=(.*)', line)
+        if match:
+            jira_key = match[0]
+            break
+    if not jira_key:
+        raise Exception('JIRA ID Not found')
+    print('JIRA ID found: {0}'.format(jira_key))
+    return jira_key
+
+
+def update_build_info(integration_change,
+                      pkg_info_file,
+                      gerrit_info_path,
+                      database_info_path,
+                      dry_run=False):
+    jira_key = get_jira_id(integration_change, gerrit_info_path)
+    mydb = mysql_api.init_from_yaml(database_info_path, server_name='skytrack')
+    mydb.init_database('skytrack')
+    knife_link_temp = 'http://5g-cb.es-si-s3-z4.eecloud.nsn-net.net' \
+                      '/BucketList/index.html?prefix=knife/{knife}/'
+    with open(pkg_info_file) as pkg_info:
+        match = re.findall(r'knife\/(.*)\/', pkg_info.read())
+        if not match:
+            raise Exception('Knife ID can not be found in {0}'.format(pkg_info_file))
+    knife_id = match[0]
+    knife_link = knife_link_temp.format(knife=knife_id)
+    search_info = mydb.executor("SELECT entity_build "
+                                "FROM t_issue WHERE issue_key = '{0}'".format(jira_key),
+                                output=True)
+    origin_entity_info = search_info[0][0]
+    entity_info = '{0}*{1}'.format(knife_id.rsplit('.', 1)[0], knife_link)
+    if origin_entity_info:
+        for line in origin_entity_info.split(','):
+            if line.split('*')[0] in knife_id:
+                continue
+            entity_info += ',{0}'.format(line)
+    if dry_run:
+        print('DRY-RUN MODE:')
+        print('Will update entity_build: {0}'.format(entity_info))
+        return
+    mydb.update_info(
+        table='t_issue',
+        replacements={
+            'entity_build': entity_info
+        },
+        conditions={
+            'issue_key': jira_key
+        }
+    )
+    print('Entity build info updated')
+    print('Entity build: {0}'.format(entity_info))
+
+
+def clean_build_info(integration_change, gerrit_info_path, database_info_path, dry_run=False):
+    jira_key = get_jira_id(integration_change, gerrit_info_path)
+    mydb = mysql_api.init_from_yaml(database_info_path, server_name='skytrack')
+    mydb.init_database('skytrack')
+    print('Clean up {0} entity build info'.format(jira_key))
+    if dry_run:
+        print('DRY-RUN MODE:')
+        print('entity_build, entity_build_status and entity_test_status will be cleanup')
+        return
+    mydb.update_info(
+        table='t_issue',
+        replacements={
+            'entity_build': None,
+            'entity_test_status': 0
+        },
+        conditions={
+            'issue_key': jira_key
+        }
+    )
+
+
+if __name__ == '__main__':
+    fire.Fire()
