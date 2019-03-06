197c05ce20a9f6c0bd16b312dcf738191287d499
Jiang, Lingyun-Alex (NSB - CN/Hangzhou) <lingyun-alex.jiang@nokia-sbell.com>
2019-03-06 09:32:47 +0200

    Revert "HZSCMWORK-4584: components recheck after EVN check pipeline succeed"
    This reverts commit 3e44f985c6f5a3645859efe8096057a1ef79e7f8.
    
    Change-Id: Ibc5351d380064274cdba60f47a34a23223a79fd1

--- integration/rebase_env.py
+++ integration/rebase_env.py
@@ -3,7 +3,6 @@
 import json
 import re
 import shlex
-import time
 from pprint import pprint
 
 import fire
@@ -122,11 +121,6 @@ def change_message_by_env_change(change_no, env_change_list, rest):
         return to_be_replaced, to_replace
     except Exception as e:
         print(e)
-
-
-def getting_env_check_result(rest, change_no, username):
-    change_detail = rest.get_detailed_ticket(change_no)
-    return check_user_label_from_detail(change_detail, username, 'Verified')
 
 
 def run(gerrit_info_path, change_no,
@@ -263,22 +257,6 @@ def run(gerrit_info_path, change_no,
     change_list = get_change_list_from_comments(change_info)
     print('Changes are:')
     pprint(change_list)
-
-    waitting_period = 0
-    while True:
-        if waitting_period >= 300:
-            raise Exception('Can not get ENV check pipeline result in 5 mins, please fix '
-                            'and rerun rebase_ENV')
-        env_verified = getting_env_check_result(rest, change_no, username)
-        if env_verified == 1:
-            print('ENV Verified +1, starting to recheck components')
-            break
-        elif env_verified == -1:
-            raise Exception('EVN check pipeline failed, please check your input content and'
-                            ' rerun rebase_ENV job')
-        else:
-            waitting_period += 60
-            time.sleep(60)
 
     if auto_recheck:
         # 5 recheck all changes
