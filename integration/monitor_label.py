47b38daba05dc86864e90a7c747a364275776e9d
jidan <jingjing.dan@nokia-sbell.com>
2019-06-05 11:27:22 +0300

    print log for skytrack web GUI to show
    Change-Id: Ibb1f0c7f4b1287600cccd9a1ad08960add35414f

--- integration/monitor_label.py
+++ integration/monitor_label.py
@@ -93,25 +93,6 @@ def _check_ticket_checked(ssh_server, ssh_port, ssh_user, ssh_key, ticket):
     return False
 
 
-def _check_manager_ticket_ok(ssh_server, ssh_port, ssh_user, ssh_key, ticket):
-    result = False
-    try:
-        if api.gerrit_api.does_patch_set_match_condition(
-                ssh_user, ssh_server, ticket, ['label:Integrated=-2'],
-                ssh_key, port=ssh_port):
-            raise FatalException(
-                'Manager ticket [{}] integration failed'.format(ticket))
-        result = api.gerrit_api.does_patch_set_match_condition(
-            ssh_user, ssh_server, ticket,
-            ['label:Verified=+1', 'label:Integrated=+2'],
-            ssh_key, port=ssh_port)
-    except FatalException as fe:
-        raise fe
-    except Exception as ex:
-        print('check manager ticket ok met a execption [{}]'.format(str(ex)))
-    return result
-
-
 def get_ticket_list_from_comments(info):
     json_re = re.compile(r'Tickets-List: ({.*})')
     for item in reversed(info['comments']):
@@ -183,6 +164,7 @@ def _main(ssh_server, ssh_port, ssh_user, ssh_key, change_id,
             }
         )
     print('Starting check if all tickets are done...')
+    skytrack_log_collector = []
     try:
         # update depends list
         depends_list = update_depends_list(rest, targets['manager'])
@@ -253,6 +235,8 @@ def _main(ssh_server, ssh_port, ssh_user, ssh_key, change_id,
             item['status'] = _check_ticket_ok(ssh_server, ssh_port,
                                               ssh_user,
                                               ssh_key, item['ticket'], ticket_project)
+            if not item['status'] and item['attached']:
+                skytrack_log_collector.append('Change {} does not get Verified+1 or Code Review+1/+2 label'.format(item['ticket']))
             print('Ticket {} pass status: {}'.format(
                 item['ticket'], item['status']))
     except Exception as ex:
@@ -261,58 +245,14 @@ def _main(ssh_server, ssh_port, ssh_user, ssh_key, change_id,
     sys.stderr.flush()
 
     if _if_checklist_all_pass(pass_list):
-        print('All ticket are done. Labeling manager ticket...')
-        retry_time = 10
-        while retry_time > 0:
-            try:
-                api.gerrit_api.review_patch_set(ssh_user, ssh_server, targets['manager'],
-                                                ['Verified=+1', 'Integrated=-1'],
-                                                'Set labels for integration', ssh_key,
-                                                port=ssh_port)
-                api.gerrit_api.review_patch_set(ssh_user, ssh_server, targets['manager'],
-                                                [], 'reintegrate', ssh_key, port=ssh_port)
-                break
-            except Exception as ex:
-                print('Labeling met an execption [{}]'.format(str(ex)))
-                retry_time -= 1
-                time.sleep(10)
-        print('Check if manager ticket is done with integration...')
-        while not _check_manager_ticket_ok(
-                ssh_server, ssh_port, ssh_user, ssh_key, targets['manager']):
-            sys.stdout.flush()
-            sys.stderr.flush()
-            time.sleep(30)
-        print('Integration test done, make root and manager ticket into gating...')
-        # api.gerrit_api.review_patch_set(ssh_user, ssh_server, targets['manager'],
-        #                                 ['Code-Review=+2'], None,
-        #                                 ssh_key, port=ssh_port)
-        # api.gerrit_api.review_patch_set(ssh_user, ssh_server, targets['root'],
-        #                                 ['Verified=+1', 'Integrated=+2',
-        #                                  'Code-Review=+2'], None,
-        #                                 ssh_key, port=ssh_port)
-        retry_time = 10
-        while retry_time > 0:
-            try:
-                api.gerrit_api.review_patch_set(ssh_user, ssh_server, targets['root'],
-                                                ['Verified=+1', 'Integrated=+2'], None,
-                                                ssh_key, port=ssh_port)
-
-                if auth_type == 'basic':
-                    rest.change_to_basic_auth()
-                elif auth_type == 'digest':
-                    rest.change_to_digest_auth()
-
-                rest_id = rest.get_ticket(targets['manager'])['id']
-                rest.review_ticket(rest_id, 'Make into gate', {'Code-Review': 2})
-                break
-                # rest_id = rest.get_ticket(targets['root'])['id']
-                # rest.review_ticket(rest_id, 'Make into gate', {'Code-Review': 2})
-            except Exception as ex:
-                print('Labeling met an execption [{}]'.format(str(ex)))
-                retry_time -= 1
-                time.sleep(10)
+        print('All component changes match the verified+1 and code review+1/+2 requirement.')
     else:
         print('please check the tickets listed above, make sure the requirement achieved, then retry')
+        if len(skytrack_log_collector) > 0:
+            print('integration framework web output start')
+            for log in skytrack_log_collector:
+                print(log)
+            print('integration framework web output end')
         sys.exit(1)
 
 
