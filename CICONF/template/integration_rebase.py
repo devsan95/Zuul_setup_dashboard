42302bad3097082110bf2528871dddf4e72dcaf9
y75zhang <yang-mark.zhang@nokia-sbell.com>
2019-07-16 17:11:18 +0800

    add rebase doc for gerrit to rebase email
    Change-Id: I2b067da4d00105eabeccf04cce83afdb4f20fad6

--- CICONF/template/integration_rebase.py
+++ CICONF/template/integration_rebase.py
@@ -4,6 +4,7 @@ mail_object = {'Integration {topic} switch to {mode}': [
         ('Baseline package', '{base_package}'),
         ('Release Date', '[function]get_date_time')]},
     {'Rebase Result': '{rebase_result}'},
+    {'How to rebase': '[link]https://confluence.int.net.nokia.com/display/BTSSCMHGH/How+to+rebase+gerrit+change'},
     {'5G HZ SCM Contact': [('I_5G_CB_SCM_GMS', '[mail]I_5G_CB_SCM@internal.nsn.com')]}]}
 
 mandatory_params = ['topic', 'base_package']
