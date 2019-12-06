2586bf1599315921a9995bd1cf5432feb96413b4
yizheng <irin.zheng@nokia-sbell.com>
2019-12-06 10:32:11 +0800

    remove merger check add jenkins and nginx_set
    Change-Id: Ice55b7c8aba2738ae7eefcf90e0b2f8c838ff633

--- service-monitor/container_status_check.sh
+++ service-monitor/container_status_check.sh
@@ -34,16 +34,20 @@ if test -n "$exited_containers_info"; then
             restart_container_and_log
         elif [ x"$container_name" == x"zuul-server-lte" ]; then
             restart_container_and_log
-        elif [ x"$container_name" == x"zuul-merger" ]; then
-            if [ x"$(hostname)" == x"eslinb40.emea.nsn-net.net" ]; then
-                echo "<font size='5'><font color='red'><b>$container_name</b></font> container at <font color='red'><b>$(hostname)</b></font> is exited, due to this is special container, can't restart it, please check ASAP!</font><br/>" >>$(hostname).log
-            else
-                restart_container_and_log
-            fi
-        elif [[ x"$container_name" =~ ^xmerger_.*_[1-9]*$ ]]; then
-            restart_container_and_log
+        #elif [ x"$container_name" == x"zuul-merger" ]; then
+        #    if [ x"$(hostname)" == x"eslinb40.emea.nsn-net.net" ]; then
+        #        echo "<font size='5'><font color='red'><b>$container_name</b></font> container at <font color='red'><b>$(hostname)</b></font> is exited, due to this is special container, can't restart it, please check ASAP!</font><br/>" >>$(hostname).log
+        #    else
+        #        restart_container_and_log
+        #    fi
+        #elif [[ x"$container_name" =~ ^xmerger_.*_[1-9]*$ ]]; then
+        #    restart_container_and_log
         elif [ x"$container_name" == x"mysql" ]; then
             restart_container_and_log
+	elif [ x"$container_name" == x"nginx_set" ]; then
+	    restart_container_and_log
+	elif [ x"$container_name" == x"jenkins-prod" ]; then
+	    restart_container_and_log
         fi
     done
 else
