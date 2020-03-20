61e271527e780b2cb7e4cef347761e898c623546
taowang <tao.8.wang@nokia-sbell.com>
2020-03-20 09:46:47 +0200

    [CBSCMS-10820][CBSCMS-10821]:RCPvDUINT trigger always success when knife build is failed
    RCPvDUINT trigger env change creation can not based on specific commit as base.
    
    Change-Id: I935eaf8594fdab2d00569ad2a6a2f63b5e8186c9

--- integration/rcpvduint_trigger.py
+++ integration/rcpvduint_trigger.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 import os
-import sys
 import re
 import codecs
 import time
 import json
 import argparse
-import logging
 import jenkins
 import requests
-from api import gerrit_rest
+from api import gerrit_rest, log_api
 from scm_tools.wft.api import WftAPI
 
 
+log = log_api.get_console_logger("rcpvduint_knife")
 wft = WftAPI()
 integration = 'MN/5G/COMMON/integration'
 wft_api = '{}/ALL/api/v1/build.json'.format(os.environ['WFT_API_URL'])
 jenkins_server = 'http://wrlinb147.emea.nsn-net.net:9090/'
 knife_job = 'Knives.START'
 mail_job = 'vran.mail_notification.CI'
+server = jenkins.Jenkins(jenkins_server)
 download_list = [
     'http://es-si-s3-z4.eecloud.nsn-net.net/5g-cb/BucketList/index.html?prefix=knife/',
     'http://s3-china-1.eecloud.nsn-net.net/5g-cb/BucketList/index.html?prefix=knife/'
@@ -82,12 +82,11 @@ def get_latest_build(branch, build_status=None):
         verify=False
     )
     if not response.ok:
-        log.error("get latest build from wft error!")
-        sys.exit(1)
+        raise Exception("get latest build from wft error!")
     return json.loads(response.text)['items'][0]['version']
 
 
-def create_integration_change():
+def create_integration_change(rest):
     version_filter = '''
 ,
     "1": {
@@ -133,7 +132,7 @@ def arguments():
     return parse.parse_args()
 
 
-def update_env_file():
+def update_env_file(rest, version_dict, change_id):
     env_path = 'env/env-config.d/ENV'
     env_content = rest.get_file_content(env_path, change_id)
     log.debug(env_content)
@@ -146,8 +145,7 @@ def update_env_file():
                 env_content
             )
         else:
-            log.error("Can not find {} from env file!".format(item))
-            sys.exit(1)
+            raise Exception("Can not find {} from env file!".format(item))
     rest.add_file_to_change(change_id, env_path, env_content)
     rest.publish_edit(change_id)
     commit_hash = rest.get_commit(change_id)['commit']
@@ -155,7 +153,7 @@ def update_env_file():
     return commit_hash
 
 
-def generate_releasenote():
+def generate_releasenote(version_dict, latest_build, tar_pkg, build_url, description):
     json_file = '5G_{}.json'.format(
         tar_pkg.replace('knifeanonymous', 'RCPVDUINT')
     )
@@ -211,9 +209,9 @@ def generate_releasenote():
     wft.json(json_file)
 
 
-def trigger_mail_job():
+def trigger_mail_job(mails, tar_pkg):
     params = {
-        'mails': args.mails,
+        'mails': mails,
         'baseline': "5G_{}".format(
             tar_pkg.replace('knifeanonymous', 'RCPVDUINT')
         ),
@@ -229,16 +227,34 @@ def trigger_jenkins_job(job_name, params, interval_time):
         parameters=params,
         token='secretOnlyKnownByWFT'
     )
+    try_time = 0
     while True:
-        queue_info = server.get_queue_item(queue_id)
+        try:
+            queue_info = server.get_queue_item(queue_id)
+        except Exception:
+            try_time += 1
+            if try_time > 200:
+                raise Exception("Get queue_info error,no more tries left")
+            log.warining("Get queue_info error, try again.")
+            time.sleep(20)
+            continue
         if 'executable' in queue_info:
             build_id = queue_info['executable']['number']
             break
-        time.sleep(90)
+        time.sleep(70)
     log.info("{} build #{} start".format(job_name, build_id))
     build_info = dict()
+    try_time = 0
     while True:
-        build_info = server.get_build_info(job_name, build_id)
+        try:
+            build_info = server.get_build_info(job_name, build_id)
+        except Exception:
+            try_time += 1
+            if try_time > 400:
+                raise Exception("Get build_info error,no more tries left")
+            log.warining("Get build_info error, try again.")
+            time.sleep(30)
+            continue
         if not build_info['building']:
             break
         time.sleep(interval_time)
@@ -246,27 +262,11 @@ def trigger_jenkins_job(job_name, params, interval_time):
     return build_id, build_info
 
 
-def setup_logger(debug="False"):
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        '%(asctime)s %(levelname)s:\t%(module)s@%(lineno)s:\t%(message)s'
-    )
-    ch = logging.StreamHandler()
-    if debug.lower() == "true":
-        ch.setLevel(logging.DEBUG)
-    else:
-        ch.setLevel(logging.WARNING)
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-    return logger
-
-
-def trigger_knife_job():
+def trigger_knife_job(rest, mails, latest_build, change_hash, change_id):
     desc = None
     url = None
     params = {
-        'MAIL_LIST': args.mails,
+        'MAIL_LIST': mails,
         'KNIFE_JSON': '{ "integration": {"repo_ver": "%s"} }' % change_hash,
         'FORCED_LABEL': 'cores-beast-knife',
         'BASE_PACKAGE': latest_build.replace('5G_', '')
@@ -284,6 +284,9 @@ def trigger_knife_job():
     if build_info['result'] == 'SUCCESS':
         desc = build_info['description']
         url = build_info['url']
+    else:
+        rest.abandon_change(change_id)
+        raise Exception("Knife job failed: {}".format(build_info['url']))
     return desc, url, tar_pkg_name
 
 
@@ -300,16 +303,30 @@ def get_version_dict(versions):
     return ver_dict
 
 
-if __name__ == "__main__":
+def main():
     args = arguments()
-    server = jenkins.Jenkins(jenkins_server)
-    log = setup_logger(debug="True")
     rest = gerrit_rest.init_from_yaml(args.gerrit)
     version_dict = get_version_dict(args.changes)
-    change_id, latest_build = create_integration_change()
-    change_hash = update_env_file()
-    description, build_url, tar_pkg = trigger_knife_job()
+    change_id, latest_build = create_integration_change(rest)
+    change_hash = update_env_file(rest, version_dict, change_id)
+    description, build_url, tar_pkg = trigger_knife_job(
+        rest,
+        args.mails,
+        latest_build,
+        change_hash,
+        change_id
+    )
     if description and build_url:
-        generate_releasenote()
-        trigger_mail_job()
+        generate_releasenote(
+            version_dict,
+            latest_build,
+            tar_pkg,
+            build_url,
+            description
+        )
+        trigger_mail_job(args.mails, tar_pkg)
     rest.abandon_change(change_id)
+
+
+if __name__ == "__main__":
+    main()
