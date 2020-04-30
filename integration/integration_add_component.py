61c84cda7b7482fbb78e5b3d94c4629b165b2109
lingyuji <lingyun-alex.jiang@nokia-sbell.com>
2020-04-30 16:24:07 +0800

    fix update commit message in closed changes issue
    Change-Id: I29084ebfb03d0d87cf4caaa55c241733cd1e775a

--- integration/integration_add_component.py
+++ integration/integration_add_component.py
@@ -260,6 +260,8 @@ def main(root_change, comp_name, component_config, gerrit_info_path, mysql_info_
         add_tmp_file(rest, comp_change_number, comp['files'], root['topic'])
     if comp_name in depends_components:
         for comp_change in root['component_changes']:
+            if rest.get_ticket(comp_change)['status'] in ['MERGED', 'ABANDONED']:
+                continue
             add_depends_info(rest, comp_change, depends_change=comp_change_number)
 
     int_operator = operate_int.OperateIntegrationChange(gerrit_info_path, root['manager_change'], mysql_info_path)
