7fb9de58f74091af198ac4f1e36ada61516f17f3
rihu <richard.1.hu@nokia-sbell.com>
2020-05-14 10:01:26 +0300

    update disk-space-moniter-all.sh to 90%
    Change-Id: I00d8433298ef2a50c48f755c3c432cce1e34b80b

--- tools/disk-space-moniter-all.sh
+++ tools/disk-space-moniter-all.sh
@@ -6,13 +6,16 @@ hostname=`hostname`
 #exceptlist="except.list"
 #lessspace=2097152
 lessspace=90
-df -h | awk '{print $5$6}' > used.list
-cat used.list
+df -lh | awk '{print $5$6}' | egrep -v "(/build|/build/rcp|/tmp_opt|/ltesdkmguit|/build2|/build/WindRiver|/build/ltesdkroot)" > used.list
+df -h /home/ca_5g_hz_scm | awk '{print $5$6}' | egrep -v "(/build|/build/rcp|/tmp_opt|/ltesdkmguit|/build2|/build/WindRiver|/build/ltesdkroot)" | grep "ca_5g_hz_scm" >> used.list
 sed -i '1d' used.list
+cat used.list
 while read line
 do
     Mounted=`echo ${line#*%}`
     Use=`echo ${line%%%*}`
+    echo $Use
+    if [ "$Mounted" != "ounted" ] && [ "$Use" != "var" ];then
     if [ -f "diskexcept.list" ];then
         let "i=0"
         while read line1
@@ -30,7 +33,7 @@ do
             if [ $Use -le $lessspace ];then
                 echo "This $Mounted disk space looks well for now."
             else
-                echo "[WARNING] $Mounted disk space on $hostname SPACE ALARM, more than 90%!!!"
+                echo "[WARNING] $Mounted disk space on $hostname SPACE ALARM, more than 95%!!!"
                 df -h
                 echo $Mounted >> servername_$hostname
                 #scp servername_$hostname root@10.157.164.206:/ephemeral/workspace/workspace/SERVERS_DISK_MONITOR/
@@ -47,8 +50,11 @@ do
             #scp servername_$hostname root@10.157.164.206:/ephemeral/workspace/workspace/SERVERS_DISK_MONITOR/
         fi
     fi
+    fi
 done < used.list
 rm -f used.list
 if [ -f "servername_$hostname" ];then
-    scp servername_$hostname root@10.157.164.203:/root/workspace/workspace/SERVERS_DISK_MONITOR/
+#    scp servername_$hostname root@10.157.164.206:/ephemeral/workspace/workspace/SERVERS_DISK_MONITOR/
+#    scp servername_$hostname ca_zuuler@10.159.11.27:/var/fpwork/workspace/workspace/SERVERS_DISK_MONITOR/
+    scp servername_$hostname root@10.157.164.203:/root/workspace/SERVERS_DISK_MONITOR/
 fi
