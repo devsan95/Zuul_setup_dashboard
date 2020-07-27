8b217e1f1d9ef53353e7ed1ce97197ff8f0d2ac1
jayac <jayanth.p@nokia.com>
2020-07-27 11:23:02 +0300

    code to validate there is server upgarde in teh weekend
    Change-Id: I3c15b3e6093e3c79770a667889d793d2f6466901

--- service-monitor/layout_validate.sh
+++ service-monitor/layout_validate.sh
@@ -25,6 +25,11 @@ tdiff=`expr $diff_time / 60`
 
 echo -e  "\n Difference in  timestamp $tdiff  minutes"
 
+if test $tdiff > 720 ; then
+   echo -e \n There is server upgrade in weekend, please check "
+   exit 0
+fi
+
 if test $tdiff -le 15 ; then
 
   echo "No issue in the synch"
