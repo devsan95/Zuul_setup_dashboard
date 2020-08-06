21db2af3015836383227ca6103c1ecfc41ccec03
rihu <richard.1.hu@nokia-sbell.com>
2020-08-06 04:41:41 +0300

    change for gerritqa&account
    Change-Id: Ie89d454d54cd36e3a69d6f1854b47b7a915a75ab

--- pipeline/validate_zuul_forbunch.py
+++ pipeline/validate_zuul_forbunch.py
@@ -137,7 +137,7 @@ def _main():
     gerrit_api.review_patch_set(
         ssh_user, ssh_server, patchset3, ['Code-Review=+2'])
 
-    auth = HTTPBasicAuth('rihu', '%264531Hx%')
+    auth = HTTPBasicAuth('cazuul', 'Welcome321')
     rest = GerritRestAPI(url="http://" + ssh_server, auth=auth, verify=False)
     gate_bunch_topic = False
     for i in range(1, 30):
