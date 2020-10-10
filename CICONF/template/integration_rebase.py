bb177a0e96987e42c7111278a3065fef33e88980
Dan Jingjing-Sophia <jingjing.dan@nokia-sbell.com>
2020-10-10 05:00:52 +0300

    update rebase guide link in the email template
    Change-Id: I42cb3753b9f052966a9b6f8ea5aa6838b72747c4

--- CICONF/template/integration_rebase.py
+++ CICONF/template/integration_rebase.py
@@ -4,7 +4,7 @@ mail_object = {'Integration {topic} switch to {mode}': [
         ('Baseline package', '{base_package}'),
         ('Release Date', '[function]get_date_time')]},
     {'Rebase Result': '{rebase_result}'},
-    {'How to rebase': '[link]https://confluence.int.net.nokia.com/display/BTSSCMHGH/How+to+rebase+gerrit+change'},
+    {'How to rebase': '[link]https://confluence.ext.net.nokia.com/display/BTSSCMHGH/06+How+to+rebase+gerrit+change'},
     {'5G HZ SCM Contact': [('I_5G_CB_SCM_GMS', '[mail]I_5G_CB_SCM@internal.nsn.com')]}]}
 
 mandatory_params = ['topic', 'base_package']
