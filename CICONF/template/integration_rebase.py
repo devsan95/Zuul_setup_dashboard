4d7ec33dc30a5234d3b2c02693692b9b3b1895fe
Dan Jingjing-Sophia <jingjing.dan@nokia-sbell.com>
2020-10-20 11:23:33 +0300

    update email link to jira link in email template
    Change-Id: Ic3c524d7540077952eecd8fe124c7abcdc7cdcc2

--- CICONF/template/integration_rebase.py
+++ CICONF/template/integration_rebase.py
@@ -5,6 +5,6 @@ mail_object = {'Integration {topic} switch to {mode}': [
         ('Release Date', '[function]get_date_time')]},
     {'Rebase Result': '{rebase_result}'},
     {'How to rebase': '[link]https://confluence.ext.net.nokia.com/display/BTSSCMHGH/06+How+to+rebase+gerrit+change'},
-    {'5G HZ SCM Contact': [('I_5G_CB_SCM_GMS', '[mail]I_5G_CB_SCM@internal.nsn.com')]}]}
+    {'5G CB SCM Contact': [('Create Jira ticket here', '[link]https://jiradc.ext.net.nokia.com/secure/CreateIssueDetails!init.jspa?pid=44082&issuetype=3&description=%0A%0A%3CPut%20details%20about%20your%20problem%20here%3E')]}]}
 
 mandatory_params = ['topic', 'base_package']
