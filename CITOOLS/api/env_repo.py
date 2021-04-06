0a0d5e65177e1291c3d49b8d49059afeaf2264a8
y75zhang <yang-mark.zhang@nokia-sbell.com>
2021-04-06 11:05:01 +0800

    FIX: env_path should not be intialed as empty
    Change-Id: I782e59788e6b64bcc5eb5f81a8db7709c5ccb934

--- CITOOLS/api/env_repo.py
+++ CITOOLS/api/env_repo.py
@@ -4,17 +4,17 @@
 
 def get_env_repo_info(rest, change_no):
     env_repo = 'MN/5G/COMMON/integration'
-    env_path = ''
+    env_path = 'env/env-config.d/ENV'
     config_yaml_file = 'config.yaml'
 
     try:
         rest.get_file_content(env_path, change_no)
-        env_path = 'env/env-config.d/ENV'
     except Exception as e:
         print('env file not in integration repo, reason:')
         print(str(e))
         try:
             rest.get_file_content(config_yaml_file, change_no)
+            env_path = ''
         except Exception as e:
             print('config yaml file not in integration repo, reason:')
             print(str(e))
