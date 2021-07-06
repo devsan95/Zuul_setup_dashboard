c6a9a219c6e602d1ef2a85f1770c9e51b2c47666
hongyu <hongyu.pan@nokia-sbell.com>
2021-07-06 05:35:02 +0300

    FCA_SCM_CI-57773 Enable functionality for user to update baseline even in HEAD mode
    Change-Id: I90badc98d22f600bee2dc628e62faa118a7fe364

---
+++ integration/update_base.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python2.7
+# -*- coding:utf-8 -*-
+import fire
+
+from api import gerrit_rest
+from api import config
+from mod.integration_change import RootChange
+
+
+CONF = config.ConfigTool()
+CONF.load('repo')
+COMP_INFO_DICT = {}
+
+
+def update_base(root_change, gerrit_info_path, base_package):
+    rest = gerrit_rest.init_from_yaml(gerrit_info_path)
+    op = RootChange(rest, root_change)
+    comp_change_list, int_change = op.get_components_changes_by_comments()
+    base_list = base_package.split(',')
+    for base_pkg_name in base_list:
+        if base_pkg_name == 'use_default_base':
+            rest.review_ticket(int_change, 'use_default_base')
+        else:
+            ver_partten = '.'.join(base_pkg_name.split('.')[0:2])
+            rest.review_ticket(
+                int_change,
+                'update_base:{},{}'.format(ver_partten, base_pkg_name))
+
+
+if __name__ == '__main__':
+    fire.Fire()
