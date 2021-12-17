d6d341a6ff9d102e257e63e8f65e130b2c07b8ce
lema <lei.1.ma@nokia-sbell.com>
2021-12-17 19:58:03 +0800

    update_base to support SBTS build name
    Change-Id: I58153e7682a248c125af40fc737d81efcd030b19

--- integration/update_base.py
+++ integration/update_base.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python2.7
 # -*- coding:utf-8 -*-
+import re
 import fire
 
 from api import gerrit_rest
@@ -10,7 +11,7 @@ from mod.integration_change import RootChange
 CONF = config.ConfigTool()
 CONF.load('repo')
 COMP_INFO_DICT = {}
-
+SBTS_RE = re.compile(r'^SBTS\d{2}(.\d)?_ENB_\d{4}_\d{6}_\d{6}$')
 
 def update_base(root_change, gerrit_info_path, base_package):
     rest = gerrit_rest.init_from_yaml(gerrit_info_path)
@@ -21,7 +22,8 @@ def update_base(root_change, gerrit_info_path, base_package):
         if base_pkg_name == 'use_default_base':
             rest.review_ticket(int_change, 'use_default_base')
         else:
-            ver_partten = '.'.join(base_pkg_name.split('.')[0:2])
+            ver_partten = base_pkg_name.split('_')[0] if SBTS_RE.match(base_pkg_name) \
+                else '.'.join(base_pkg_name.split('.')[0:2])
             rest.review_ticket(
                 int_change,
                 'update_base:{},{}'.format(ver_partten, base_pkg_name))
