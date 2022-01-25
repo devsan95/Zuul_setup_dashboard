400d69e85dee7dc9e13d2a8082a02a7222804f5f
Deepak Kumar Choudhary <deepak.kumar_choudhary@nokia.com>
2022-01-25 17:35:08 +0530

    FCA_SCM_CI-71141 Investigation - Mergers logs are missing in S3 bucket
    Change-Id: I0f82f7b48bcf6437aae117fe6c157ba676b03c09

--- tools/zuul_log_upload.sh
+++ tools/zuul_log_upload.sh
@@ -2,10 +2,31 @@
 
 set -x
 
+# Note: Set ORG value in server's environment variable as shwon below.
+# Step1: open .bash_profile
+# vi .bash_profile
+# Step2: Add the below line in .bash_profile (example for 5g).
+# export ORG=5g
+# Step3: Apply the change to bash_profile.
+# source ~/.bash_profile
+
+# If this script run in cron job then below line is required to load the environment variable.
+. ~/.bash_profile > /dev/null 2>&1
+
+# /opt/zuul/script/upload_log_to_s3.sh script is required and should be available inside docker container
+# https://gerrit.ext.net.nokia.com/gerrit/gitweb?p=MN/SCMTA/zuul/mn_scripts.git;a=blob;f=tools/upload_log_to_s3.sh;h=fcbf7bc61d7689f3e3a59270737f0509250b3947;hb=refs/heads/master
+
 SCRIPT_FOLDER=$(dirname $(readlink -f "$0"))
 
-# Change this value based on the environment.
-ORG="5g"
+# short hostname
+HOSTNAME=$(hostname -s)
+
+# Check if HOSTNAME and ORG are set in environment variable.
+if [[ -z ${HOSTNAME} ]] || [[ -z ${ORG} ]]; then
+    echo "HOSTNAME or ORG is not set"
+    exit 1
+fi
+
 
 # Check if docker daemon is running or not.
 sudo docker info > /dev/null 2>&1
@@ -24,7 +45,7 @@ if [[ ${#container_names[@]} -lt 1 ]]; then
 fi
 
 for container_name in "${container_names[@]}"; do
-    if [[ x"${container_name}" == x"zuul-server" ]] || \
+    if [[ x"${container_name}" =~ ^xzuul-server.*$ ]] || \
        [[ x"$container_name" =~ ^xmerger.*.[0-9]*$ ]] || \
        [[ x"${container_name}" == x"gearman" ]]
     then
