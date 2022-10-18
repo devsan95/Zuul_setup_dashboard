871d14879370d22485cbf281345cbc062dd789f3
y75zhang <yang-mark.zhang@nokia-sbell.com>
2022-10-18 08:50:55 +0300

    FCA_SCM_CI-87076: add build info to skytrack
    Change-Id: I84605b8006d9eab57802228b5e7ddb05aa884236

--- integration/trigger_increment_build.py
+++ integration/trigger_increment_build.py
@@ -5,19 +5,19 @@
 import fire
 import json
 import re
+import time
 from api import gerrit_rest
 from mod import integration_change
 from mod import utils
 from mod import wft_actions
+import skytrack_database_handler
 
 
 BRANCH_MAP = {'PS': 'PSINT', 'RCP': 'RCPINT', 'NIDD': 'NIDDINT', 'feature': 'FEINT'}
 PRODUCTION_TYPES = ['SBTS', 'vDUCNF', 'vCUCNF']
 
 
-def get_wft_int_branch(rest, change_id, baseline):
-    int_change = integration_change.ManageChange(rest, change_id)
-    topic_type = int_change.get_topic_type()
+def get_wft_int_branch(topic_type, baseline):
     intbranch_end = BRANCH_MAP.get(topic_type)
     if not intbranch_end:
         raise Exception('topic type {} not in map {}'.format(topic_type, BRANCH_MAP))
@@ -40,13 +40,37 @@ def get_changed_info(knife_change_file):
     return changed_dict
 
 
-def run(property_file, change_id, gerrit_info_path, knife_change_file):
+def update_build_info(database_info_path, wft_name, wft_link, jira_key, stream):
+    now_time = int(time.time()) * 1000
+    skytrack_database_handler.skytrack_detail_api(
+        integration_name=jira_key,
+        product='5G',
+        package_name=wft_name,
+        mini_branch=stream,
+        type_name='Integration Build',
+        status=1,
+        link=wft_link,
+        start_time=now_time,
+        end_time=now_time
+    )
+    build_info_in_link = "<a href='{}'>{}</a>".format(wft_link, wft_name)
+    skytrack_database_handler.update_events(
+        database_info_path=database_info_path,
+        integration_name=jira_key,
+        description="{0} integration package created: {1}".format(stream, build_info_in_link)
+    )
+
+
+def run(property_file, change_id, gerrit_info_path, knife_change_file, database_info_path):
     rest = gerrit_rest.init_from_yaml(gerrit_info_path)
     data = utils.file_to_dict(property_file)
     baseline = data['BASELINE']
     revision = data['REVISION']
     branch = data['BRANCH']
-    wft_branch = get_wft_int_branch(rest, change_id, baseline)
+    int_change = integration_change.ManageChange(rest, change_id)
+    topic_type = int_change.get_topic_type()
+    jira_id = int_change.get_jira_id()
+    wft_branch = get_wft_int_branch(topic_type, baseline)
     increment_obj = wft_actions.BuildIncrement(wft_branch=wft_branch,
                                                changed=get_changed_info(knife_change_file),
                                                base_build=baseline,
@@ -57,6 +81,7 @@ def run(property_file, change_id, gerrit_info_path, knife_change_file):
          "repository_branch": branch,
          "repository_revision": revision,
          "repository_type": "git"})
+    update_build_info(database_info_path, wft_name, wft_link, jira_id, baseline.split('_')[0])
     rest.review_ticket(change_id, 'IntegrationBuild: {} - {}'.format(wft_name, wft_link))
 
 
