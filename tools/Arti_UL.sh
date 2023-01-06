a22416c58bb55f90f4f7c4f62d9ee7b65f769bf6
Jayanth p <jayanth.p@nokia.com>
2023-01-06 09:55:37 +0200

    adding path
    Change-Id: If26a55eabeaa4b1872b61b685e0370aaae5df3e2

--- tools/Arti_UL.sh
+++ tools/Arti_UL.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
  rm -rf /var/jenkins_home/jobs/Artifacts_backup_to_S3/workspace/tools/$1
  /home/ca_5g_hz_scm/jfrog_files/jf rt dl $2 --url=http://artifactory-espoo1.ext.net.nokia.com/artifactory/ --password=AKCp5fUhzkiwCThFrJYaGkNJ3xrBS8EntU9a21WUVbLCpyyeSxbLnKPetFvtzvXS7Kj5YbA3u --flat
- s3cmd put --acl-public -v /home/ca_5g_hz_scm/jfrog_files/artifact/$1 s3://zuul-5g/$3
+./s3cmd put --acl-public -v /home/ca_5g_hz_scm/jfrog_files/artifact/$1 s3://zuul-5g/$3
