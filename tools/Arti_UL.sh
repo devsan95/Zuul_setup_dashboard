115e7ebf8c0a3f7f6668ac0f2568d2b51f0aaaad
Jayanth p <jayanth.p@nokia.com>
2023-01-10 10:18:21 +0200

    Adding s3 upload script files
    Change-Id: I6b2cc4730a89f874ea04eb3e668f26baf5523170

--- tools/Arti_UL.sh
+++ tools/Arti_UL.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
- rm -rf /var/jenkins_home/jobs/Artifacts_backup_to_S3/workspace/tools/$1
- /home/ca_5g_hz_scm/jfrog_files/jf rt dl $2 --url=http://artifactory-espoo1.ext.net.nokia.com/artifactory/ --password=AKCp5fUhzkiwCThFrJYaGkNJ3xrBS8EntU9a21WUVbLCpyyeSxbLnKPetFvtzvXS7Kj5YbA3u --flat
-/var/jenkins_home/jobs/Artifacts_backup_to_S3/workspace/tools/s3cmd put --acl-public -v /home/ca_5g_hz_scm/jfrog_files/artifact/$1 s3://zuul-5g/$3
+if test -f "jf" ; then
+ echo "Jfrog binary exist"
+else
+ s3cmd get --acl-public s3://zuul-5g/logs/jf . ; chmod 777 jf
+fi
+
+./jf rt dl $1 --url=http://artifactory-espoo1.ext.net.nokia.com/artifactory/ --password=AKCp5fUhzkiwCThFrJYaGkNJ3xrBS8EntU9a21WUVbLCpyyeSxbLnKPetFvtzvXS7Kj5YbA3u --flat
+s3cmd put --acl-public -v $2 s3://zuul-5g/$3
